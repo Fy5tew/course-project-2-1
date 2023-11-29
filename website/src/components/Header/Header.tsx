19bdc38248c611451f33e06761055285ffd07b85
Nikita <nikturchinovich@mail.ru>
2023-11-30 00:25:48 +0300

    Add header component

---
+++ website/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { Logo } from '../../elements/Logo';
+import { Navigation } from '../../elements/Navigation';
+import { Account } from '../../elements/Account';
+
+import styles from './Header.module.scss';
+
+
+export function Header() {
+    return (
+        <div className={styles.Header}>
+            <Logo />
+            <Navigation type='horizontal' />
+            <Account />
+        </div>
+    );
+}
