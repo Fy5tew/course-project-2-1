24c501b564d59074cd11a1b9db56fecc7b4d0b55
Nikita <nikturchinovich@mail.ru>
2023-12-01 21:49:44 +0300

    Fix header component adaptive

--- website/src/components/Header/Header.tsx
+++ website/src/components/Header/Header.tsx
@@ -1,18 +1,28 @@
+import { useDispatch } from "react-redux";
 import { useWindowSize } from "usehooks-ts";
+
+import { menuActions } from "../../features/menu/menuSlice";
 
 import { HeaderDesktop } from "./HeaderDesktop";
 import { HeaderMobile } from "./HeaderMobile";
 
 
+const BREAKPOINT_WIDTH = 710;
+
+
 export function Header() {
+    const dispatch = useDispatch();
     const { width } = useWindowSize();
 
-    if (width > 768) {
+    if (width < BREAKPOINT_WIDTH) {
         return (
-            <HeaderDesktop />
+            <HeaderMobile />
         );
     }
+
+    dispatch(menuActions.close());
+
     return (
-        <HeaderMobile />
+        <HeaderDesktop />
     );
 }
