1b4153b22b1d913b6d6c7ed50a861e5ab4035e82
Nikita <nikturchinovich@mail.ru>
2023-12-01 19:14:25 +0300

    Make header adaptive

--- website/src/components/Header/Header.tsx
+++ website/src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
+import { useWindowSize } from "usehooks-ts";
+
 import { HeaderDesktop } from "./HeaderDesktop";
 import { HeaderMobile } from "./HeaderMobile";
 
 
 export function Header() {
+    const { width } = useWindowSize();
+
+    if (width > 768) {
+        return (
+            <HeaderDesktop />
+        );
+    }
     return (
         <HeaderMobile />
     );
