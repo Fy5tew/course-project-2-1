40eafb30c9565dbbe0c12d24b86615872860e5eb
Nikita <nikturchinovich@mail.ru>
2023-12-01 22:00:57 +0300

    Add short type of logo

--- website/src/elements/Logo/Logo.tsx
+++ website/src/elements/Logo/Logo.tsx
@@ -1,14 +1,19 @@
 import styles from './Logo.module.scss';
 
 
-export function Logo() {
+export type LogoProps = {
+    short?: boolean,
+};
+
+
+export function Logo({ short }: LogoProps) {
     return (
         <div className={styles.Logo}>
             <img 
                 src="/logo/logo.svg" 
                 alt="" 
             />
-            <span>F5STORE</span>
+            {!short && <span>F5STORE</span>}
         </div>
     )
 }
