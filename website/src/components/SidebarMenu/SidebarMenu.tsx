aded3aec71a3f3da317ae283a84afc29152523ec
Nikita <nikturchinovich@mail.ru>
2023-12-02 00:29:17 +0300

    Update sidebar menu

--- website/src/components/SidebarMenu/SidebarMenu.tsx
+++ website/src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigationType } from 'react-router-dom';
+import { useWindowSize } from 'usehooks-ts';
 
 import { menuActions } from '../../features/menu/menuSlice';
 
+import { Logo } from '../../elements/Logo';
 import { MenuButton } from '../../elements/MenuButton';
 import { Account } from '../../elements/Account';
 import { Navigation } from '../../elements/Navigation';
@@ -11,9 +13,13 @@ import { Navigation } from '../../elements/Navigation';
 import styles from './SidebarMenu.module.scss';
 
 
+const BREAKPOINT_WIDTH = 370;
+
+
 export function SidebarMenu() {
     const dispatch = useDispatch();
     const navigationType = useNavigationType();
+    const { width } = useWindowSize();
     const isOpened = useSelector(menuActions.getOpened);
 
     useEffect(() => {
@@ -24,9 +30,10 @@ export function SidebarMenu() {
         <div className={styles.SidebarMenu} data-opened={isOpened}>
             <div className={styles.MenuContent}>
                 <div className={styles.Header}>
-                    <Account />
+                    <Logo short={width < BREAKPOINT_WIDTH} />
                     <MenuButton />
                 </div>
+                <Account />
                 <Navigation type='vertical' />
             </div>
         </div>
