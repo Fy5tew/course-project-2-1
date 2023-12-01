c8a7d5dc3457bdfa75227ab64723d0070e8b187f
Nikita <nikturchinovich@mail.ru>
2023-12-01 22:01:53 +0300

    Add short type of logo for extremly short screens

--- website/src/components/Header/HeaderMobile.tsx
+++ website/src/components/Header/HeaderMobile.tsx
@@ -1,13 +1,20 @@
+import { useWindowSize } from 'usehooks-ts';
+
 import { Logo } from '../../elements/Logo';
 import { MenuButton } from '../../elements/MenuButton';
 
 import styles from './Header.module.scss';
 
 
+const BREAKPOINT_WIDTH = 290;
+
+
 export function HeaderMobile() {
+    const { width } = useWindowSize();
+
     return (
         <div className={styles.Header}>
-            <Logo />
+            <Logo short={width < BREAKPOINT_WIDTH} />
             <MenuButton />
         </div>
     );
