1f60ea62b8643fbb52861eecf0db9a234d5b2253
Nikita <nikturchinovich@mail.ru>
2023-12-02 13:57:53 +0300

    Update account component: Add expandable part

--- website/src/components/Account/Account.tsx
+++ website/src/components/Account/Account.tsx
@@ -1,20 +1,46 @@
-import { useSelector } from "react-redux";
+import { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useToggle } from 'usehooks-ts';
 
-import { userActions } from "../../features/user/userSlice";
-import { avatarsActions } from "../../features/avatars/avatarsSlice";
+import { userActions } from '../../features/user/userSlice';
+import { avatarsActions } from '../../features/avatars/avatarsSlice';
 
-import { AccountInfo } from "../../elements/AccountInfo";
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
+
+import { AccountNavigation } from '../../elements/AccountNavigation';
+
+import { AccountInfo } from '../../elements/AccountInfo';
+
+import styles from './Account.module.scss';
 
 
 export function Account() {
     const userName = useSelector(userActions.getName);
     const avatarName = useSelector(userActions.getAvatar);
     const avatar = useSelector(avatarsActions.getAvatar(avatarName));
+
+    const accountRef = useRef(null);
+    const [ isExpanded, toggleExpanded, setExpanded ] = useToggle(false);
+
+    const clickHandler = () => {
+        toggleExpanded();
+    };
+
+    useOnClickOutside(accountRef, () => setExpanded(false))
     
     return (
-        <AccountInfo
-            userName={userName}
-            avatar={avatar}
-        />
+        <div className={styles.Account} onClick={clickHandler} ref={accountRef}>
+            <div className={styles.BasePart}>
+                <AccountInfo
+                    userName={userName}
+                    avatar={avatar}
+                />
+            </div>
+            <div className={styles.ExpandablePart} data-expanded={isExpanded}>
+                <AccountNavigation 
+                    type='vertical'
+                />
+            </div>
+        </div>
     );
 }
