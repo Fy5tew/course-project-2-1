bd0cece3521b3c7bd444a4942ddd6e38a70836e4
Nikita <nikturchinovich@mail.ru>
2023-12-02 12:09:32 +0300

    Now account elements take data from store

--- website/src/elements/Account/Account.tsx
+++ website/src/elements/Account/Account.tsx
@@ -1,11 +1,20 @@
+import { useSelector } from 'react-redux';
+
+import { userActions } from '../../features/user/userSlice';
+import { avatarsActions } from '../../features/avatars/avatarsSlice';
+
 import styles from './Account.module.scss';
 
 
 export function Account() {
+    const userName = useSelector(userActions.getName);
+    const avatarName = useSelector(userActions.getAvatar);
+    const avatar = useSelector(avatarsActions.getAvatar(avatarName));
+
     return (
         <div className={styles.Account}>
-            <span>Гость</span>
-            <img src='/images/avatars/guest.png' alt='' />
+            <span>{userName}</span>
+            <img src={avatar.src} alt={avatar.name} />
         </div>
     );
 }
