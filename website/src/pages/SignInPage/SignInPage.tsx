4539a22a2038d9d3d6b9e8a1cdde91dcf3aa9a9b
Nikita <nikturchinovich@mail.ru>
2023-12-03 00:14:40 +0300

    Add fake auth to /signin

--- website/src/pages/SignInPage/SignInPage.tsx
+++ website/src/pages/SignInPage/SignInPage.tsx
@@ -1,10 +1,25 @@
+import { useEffectOnce } from "usehooks-ts";
+import { useDispatch } from "react-redux";
+
 import { useTitle } from "../../hooks/useTitle";
+
+import { userActions } from "../../features/user/userSlice";
 
 import { PageLayout } from "../../components/PageLayout";
 
 
 export function SignInPage() {
     useTitle('Вход');
+
+    const dispatch = useDispatch();
+
+    useEffectOnce(() => {
+        dispatch(userActions.authorize({
+            name: 'Fy5tew',
+            email: 'nik@gmail.com',
+            password: 'hjwqvabvepawepaiyg278',
+        }));
+    });
 
     return (
         <PageLayout>
