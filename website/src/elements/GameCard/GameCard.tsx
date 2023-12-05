47adc4bbb47fe57dc91df9c5d1ee2d560e3734fd
Nikita <nikturchinovich@mail.ru>
2023-12-05 12:09:58 +0300

    Added game card element

---
+++ website/src/elements/GameCard/GameCard.tsx
@@ -0,0 +1,57 @@
+import { useRef, useEffect } from 'react';
+import { useHover, useToggle } from 'usehooks-ts';
+
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
+
+import { Game } from '../../features/games/gamesSlice';
+
+import styles from './GameCard.module.scss';
+import { Link } from 'react-router-dom';
+
+
+export type GameCardProps = {
+    game: Game,
+}
+
+
+export function GameCard({ game }: GameCardProps) {
+    const [ isExpanded, toggleExpanded, setExpanded ] = useToggle(false);
+    const cardRef = useRef<HTMLDivElement>(null);
+    const isHovering = useHover(cardRef);
+
+    useEffect(() => {
+        setExpanded(isHovering);
+    }, [setExpanded, isHovering]);
+
+    useOnClickOutside(cardRef, () => {
+        setExpanded(false);
+    })
+
+    const clickHandler = () => {
+        toggleExpanded();
+    };
+
+    return (
+        <div className={styles.GameCard} onClick={clickHandler} ref={cardRef}>
+            <img src={game.media.cover} alt='' />
+            <div className={styles.Head}>
+                <h3>{game.title}</h3>
+            </div>
+            <div className={styles.Expandable} data-expanded={isExpanded}>
+                <table className={styles.Info}>
+                    <tbody>
+                        <tr>
+                            <th>Дата выхода:</th>
+                            <td>{game.releaseDate}</td>
+                        </tr>
+                        <tr>
+                            <th>Жанры:</th>
+                            <td>{game.genres.join(', ')}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Link to={`/game/${game.id}`}>Перейти к игре {'>'}</Link>
+            </div>
+        </div>
+    );
+}
