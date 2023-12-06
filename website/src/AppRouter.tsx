3d80f13703a6c2b04cce4260765f09d319bb2031
Nikita <nikturchinovich@mail.ru>
2023-12-06 12:25:46 +0300

    Updated page routers

---
+++ website/src/AppRouter.tsx
@@ -0,0 +1,63 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { IndexPage } from './pages/IndexPage';
+import { StorePage } from './pages/StorePage';
+import { LibraryPage } from './pages/LibraryPage';
+import { GamePage } from './pages/GamePage';
+import { AccountPage } from './pages/AccountPage';
+import { AccountEditPage } from './pages/AccountEditPage';
+import { SignInPage } from './pages/SignInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { SignOutPage } from './pages/SignOutPage';
+
+
+export function AppRouter() {
+    return (
+        <Routes>
+            <Route 
+                path='/' 
+                element={ <Navigate replace to='/home' /> }
+            />
+            <Route 
+                path='home'
+                element={ <IndexPage /> }
+            />
+            <Route 
+                path='store' 
+                element={ <StorePage /> } 
+            />
+            <Route 
+                path='library' 
+                element={ <LibraryPage /> } 
+            />
+            <Route 
+                path='game'
+                element={ <Navigate to='/store' /> } 
+            />
+            <Route 
+                path='game/:gameId' 
+                element={ <GamePage /> } 
+            />
+            <Route 
+                path='account' 
+                element={ <AccountPage /> } 
+            />
+            <Route 
+                path='account/edit' 
+                element={ <AccountEditPage /> } 
+            />
+            <Route 
+                path='signin' 
+                element={ <SignInPage /> } 
+            />
+            <Route 
+                path='signup' 
+                element={ <SignUpPage /> } 
+            />
+            <Route 
+                path='signout' 
+                element={ <SignOutPage /> } 
+            />
+        </Routes>
+    );
+}
--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import { store, persistor } from './features/store';
 
-import { IndexPage } from './pages/IndexPage';
-import { StorePage } from './pages/StorePage';
-import { LibraryPage } from './pages/LibraryPage';
-import { AccountPage } from './pages/AccountPage';
-import { AccountEditPage } from './pages/AccountEditPage';
-import { SignInPage } from './pages/SignInPage';
-import { SignUpPage } from './pages/SignUpPage';
-import { SignOutPage } from './pages/SignOutPage';
+import { AppRouter } from './AppRouter';
 
 import './index.scss';
 
@@ -28,42 +21,7 @@ root.render(
     <Provider store={store}>
       <PersistGate persistor={persistor} >
         <BrowserRouter>
-          <Routes>
-            <Route path='/'>
-              <Route 
-                index 
-                element={ <IndexPage /> }
-              />
-              <Route 
-                path='store' 
-                element={ <StorePage /> } 
-              />
-              <Route 
-                path='library' 
-                element={ <LibraryPage /> } 
-              />
-              <Route 
-                path='account' 
-                element={ <AccountPage /> } 
-              />
-              <Route 
-                path='account/edit' 
-                element={ <AccountEditPage /> } 
-              />
-              <Route 
-                path='signin' 
-                element={ <SignInPage /> } 
-              />
-              <Route 
-                path='signup'
-                element={ <SignUpPage /> } 
-              />
-              <Route
-                path='signout'
-                element={ <SignOutPage /> }
-              />
-            </Route>
-          </Routes>
+          <AppRouter />
         </BrowserRouter>
       </PersistGate>
     </Provider>
