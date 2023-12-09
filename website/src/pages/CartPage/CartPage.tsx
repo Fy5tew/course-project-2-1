bd98bed8f88bf5fd039ebcb1725932255d7ba5be
Nikita <nikturchinovich@mail.ru>
2023-12-09 17:31:13 +0300

    Added cart functionality

--- website/src/pages/CartPage/CartPage.tsx
+++ website/src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSnackbar } from 'notistack';
 
 import { useTitle } from '../../hooks/useTitle';
 
@@ -18,10 +19,21 @@ import styles from './CartPage.module.scss';
 export function CartPage() {
     useTitle('Корзина');
 
+    const dispatch = useDispatch();
+    const { enqueueSnackbar } = useSnackbar();
+
     const games = useSelector(gamesActions.getAllGames);
     const cart = useSelector(authActions.getCart);
 
     const filteredGames = games.filter(game => cart.includes(game.id));
+
+    const totalPrice = Math.round(
+        filteredGames
+            .map(game => game.price)
+            .reduce((prev, current) => prev + current, 0) * 100,
+    ) / 100;
+    const taxPrice = Math.round(totalPrice * 0.05 * 100) / 100;
+    const finalPrice = Math.round((totalPrice + taxPrice) * 100) / 100;
 
     if (!filteredGames.length) {
         return (
@@ -37,15 +49,67 @@ export function CartPage() {
         );
     }
 
+    const buyButtonClickHandler = () => {
+        filteredGames.forEach(game => dispatch(authActions.addToLibrary(game.id)));
+        enqueueSnackbar<'success'>('Игры добавлены в вашу библиотеку', {
+            variant: 'success',
+        });
+    };
+
+    const clearCartButtonClickHandler = () => {
+        filteredGames.forEach(game => dispatch(authActions.removeFromCart(game.id)));
+        enqueueSnackbar<'error'>('Корзина очищена', {
+            variant: 'error',
+        });
+    };
+
     return (
         <PageLayout auth='auth'>
             <div className={styles.Cart}>
                 <div className={styles.Games}>
-                    <h2>Товары в корзине ({cart.length})</h2>
+                    <h2>Товары в корзине ({filteredGames.length})</h2>
                     <GameList games={filteredGames} showPrice showMetacriticScore />
                 </div>
                 <div className={styles.Sidebar}>
                     <h2>Описание заказа</h2>
+                    <ul>
+                        <li>
+                            <span className={styles.Title}>Количество товаров</span>
+                            <span>{filteredGames.length}</span>
+                        </li>
+                        <li>
+                            <span className={styles.Title}>Цена</span>
+                            <span>0</span>
+                        </li>
+                        {filteredGames.map((game) => (
+                            <li key={game.id}>
+                                <div className={styles.Separator}></div>
+                                <span>+ ${game.price}</span>
+                            </li>
+                        ))}
+                        <li>
+                            <span className={styles.Title}>Суммарно</span>
+                            <span>${totalPrice}</span>
+                        </li>
+                        <li>
+                            <span className={styles.Title}>Налог (5%)</span>
+                            <span>+ ${taxPrice}</span>
+                        </li>
+                        <li>
+                            <span className={styles.Title}>Итого</span>
+                            <span className={styles.FinalPrice}>${finalPrice}</span>
+                        </li>
+                    </ul>
+                    <div className={styles.Controls}>
+                        <button className={styles.BuyButton} onClick={buyButtonClickHandler}>
+                            <img src='/icons/card.svg' alt='' />
+                            Оплатить заказ
+                        </button>
+                        <button className={styles.ClearCartButton} onClick={clearCartButtonClickHandler}>
+                            <img src='/icons/cross-circle-red.svg' alt='' />
+                            Очистить корзину
+                        </button>
+                    </div>
                 </div>
             </div>
         </PageLayout>
